caa9d9e0a7617da7579776d7869e8d87c3f831d6
Rafael Cano <rafael.cano@ucsp.edu.pe>
2023-11-06 21:55:11 -0500

    adding tmux config

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ rm -rf ~/.config/nvim/lua/custom/
 ln -s $(readlink -f ./nvchad/custom) ~/.config/nvim/lua/
 ln -s $(readlink -f ./alacritty/alacritty.yml) ~/.config/alacritty/alacritty.yml
 ln -s $(readlink -f ./alacritty/themes) ~/.config/alacritty/themes
+ln -s $(readlink -f ./.tmux.conf) ~/.tmux.conf
